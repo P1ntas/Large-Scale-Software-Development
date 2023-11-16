eee930b71887a8de09b7ab1ed43a1857f57fff99
Francisco <fmamp24@gmail.com>
2023-11-16 23:33:23 +0000

    fix: fixed formatting

--- src/dinasore/resources/function_blocks/MES/MOVING_AVERAGE_V2.py
+++ src/dinasore/resources/function_blocks/MES/MOVING_AVERAGE_V2.py
@@ -5,12 +5,14 @@ class MOVING_AVERAGE_V2:
 
     def schedule(self, event_name, event_value, window, sensor_id, value):
         if event_name == 'INIT':
-            self.values_dict[sensor_id] = []  # Initialize an empty list for the sensor_id
+            # Initialize an empty list for the sensor_id
+            self.values_dict[sensor_id] = []
             return [event_value, None, 0, 0]
 
         elif event_name == 'RUN':
             if sensor_id not in self.values_dict:
-                self.values_dict[sensor_id] = []  # Initialize an empty list for the sensor_id if it doesn't exist
+                # Initialize an empty list for the sensor_id if it doesn't exist
+                self.values_dict[sensor_id] = []
             # Appends the value to the list for the corresponding sensor_id
             self.values_dict[sensor_id].append(value)
             values_list = self.values_dict[sensor_id]
@@ -24,5 +26,6 @@ class MOVING_AVERAGE_V2:
                 return [None, event_value, sensor_id, moving_average]
             else:
                 # The list doesn't have the correct size for the sensor_id
-                print(f"Sensor {sensor_id} doesn't have enough values: {len(values_list)}/{window}")
+                print(
+                    f"Sensor {sensor_id} doesn't have enough values: {len(values_list)}/{window}")
                 return [None, None, 0, 0]
--- src/dinasore/resources/function_blocks/MES/PROMETHEUS_WRITER.py
+++ src/dinasore/resources/function_blocks/MES/PROMETHEUS_WRITER.py
@@ -15,8 +15,10 @@ class PROMETHEUS_WRITER:
             try:
                 self.registry = CollectorRegistry()
 
-                self.SENSOR_VALUE = Gauge(value_name_2, value_name_2_descr, [value_name_1], registry=self.registry)
-                self.MOVING_AVERAGE = Gauge(value_name_3, value_name_3_descr, [value_name_1], registry=self.registry)
+                self.SENSOR_VALUE = Gauge(value_name_2, value_name_2_descr, [
+                                          value_name_1], registry=self.registry)
+                self.MOVING_AVERAGE = Gauge(value_name_3, value_name_3_descr, [
+                                            value_name_1], registry=self.registry)
                 self.JOB_NAME = job_name
                 self.PUSHGATEWAY_ADDRESS = f"{host}:{port}"
                 print('Set up prometheus db sucessfully')
@@ -27,9 +29,12 @@ class PROMETHEUS_WRITER:
 
         elif event_name == 'RUN':
             try:
-                self.SENSOR_VALUE.labels(**{value_name_1: str(value_1)}).set(value_2)
-                self.MOVING_AVERAGE.labels(**{value_name_1: str(value_1)}).set(value_3)
-                push_to_gateway(self.PUSHGATEWAY_ADDRESS, job=self.JOB_NAME, registry=self.registry)
+                self.SENSOR_VALUE.labels(
+                    **{value_name_1: str(value_1)}).set(value_2)
+                self.MOVING_AVERAGE.labels(
+                    **{value_name_1: str(value_1)}).set(value_3)
+                push_to_gateway(self.PUSHGATEWAY_ADDRESS,
+                                job=self.JOB_NAME, registry=self.registry)
             except Exception as e:
                 print(f"Failed to push metrics: {e}")
 
